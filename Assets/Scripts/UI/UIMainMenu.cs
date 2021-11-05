4f68e2f42463241ebe1c443a6fd860f380443d87
Levi Huillet <levimhuillet@gmail.com>
2021-11-04 22:47:31 -0500

    Main Menu added

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Grapplnyth {
+    public class UIMainMenu : MonoBehaviour {
+
+        #region Editor
+
+        [SerializeField]
+        private Button m_playGameButton;
+        [SerializeField]
+        private Button m_settingsButton;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Awake() {
+            m_playGameButton.onClick.AddListener(HandlePlayGame);
+            m_settingsButton.onClick.AddListener(HandleSettings);
+        }
+
+        #endregion
+
+        #region ButtonHandlers
+
+        private void HandlePlayGame() {
+            SceneManager.LoadScene("Labyrinth");
+        }
+        private void HandleSettings() {
+
+        }
+
+        #endregion
+    }
+}
