353be4eda5207ab96792822fd7e57ebf5d8a3439
Travis <tbird3@wisc.edu>
2021-12-04 22:08:19 -0600

    Game now once again uses level generation instead of test segments

--- Assets/Scripts/Gameplay/RotateGrappleVisual.cs
+++ Assets/Scripts/Gameplay/RotateGrappleVisual.cs
@@ -2,32 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Grapplynth {
-    public class RotateGrappleVisual : MonoBehaviour {
+namespace Grapplynth
+{
+    public class RotateGrappleVisual : MonoBehaviour
+    {
         [SerializeField] private GrappleGun grappleGun;
 
         private Quaternion desiredRotation;
         private float rotationSpeed = 10;
 
-    private Vector3 startingRotation;
+        private Vector3 startingRotation;
 
-    private void Start()
-    {
-        startingRotation = transform.localEulerAngles;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (grappleGun.IsGrappling == false)
+        private void Start()
         {
-            desiredRotation = Quaternion.Euler(startingRotation);
-            transform.localRotation = Quaternion.Lerp(transform.localRotation, desiredRotation, Time.deltaTime * rotationSpeed);
+            startingRotation = transform.localEulerAngles;
         }
-        else
+
+        // Update is called once per frame
+        void Update()
         {
-            desiredRotation = Quaternion.LookRotation(grappleGun.GetGrapplePoint - transform.position);
-            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+            if (grappleGun.IsGrappling == false)
+            {
+                desiredRotation = Quaternion.Euler(startingRotation);
+                transform.localRotation = Quaternion.Lerp(transform.localRotation, desiredRotation, Time.deltaTime * rotationSpeed);
+            }
+            else
+            {
+                desiredRotation = Quaternion.LookRotation(grappleGun.GetGrapplePoint - transform.position);
+                transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+            }
         }
     }
 }
